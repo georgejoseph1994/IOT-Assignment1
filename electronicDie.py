4c03e390df1868360e27b4c6f7416173b23cfba0
rahul94paul <rahul94.paul@gmail.com>
2020-03-23 13:55:25 +1100

    Added shake animation

--- electronicDie.py
+++ electronicDie.py
@@ -6,7 +6,9 @@ class ElectronicDie:
 
     def __init__(self):
         self.value = None
-        self.values = ['1', '2', '3', '4', '5', '6']
+        X = [0, 255, 0]  
+        O = [0, 0, 0] 
+        self.values = self.updateValues(X, O) 
         self.sense = SenseHat()
 
     def checkForShake(self, message):
@@ -15,13 +17,110 @@ class ElectronicDie:
         while flag==False:
             x, y, z = self.sense.get_accelerometer_raw().values()
             if abs(x) > 2 or abs(y) > 2 or abs(z) > 2:
-                self.sense.show_letter(choice(self.values), [0, 255, 0])
+                self.animateShaking()
+                self.value=choice(self.values)
+                self.sense.set_pixels(self.value['pixels'])
                 flag=True
-                sleep(5)
-                self.sense.clear();
+                sleep(2)
+                return self.value['number']
+    
+    def animateShaking(self):
+        X = [155, 155, 0]  
+        O = [0, 0, 0]
+        self.values = self.updateValues(X, O) 
+        i=0
+        while i<4:
+            for item in self.values: 
+                self.sense.set_pixels(item['pixels'])
+                sleep(0.1)
+            i+=1
+        X = [0, 255, 0]  
+        O = [0, 0, 0] 
+        self.values = self.updateValues(X, O) 
+
+    def updateValues(self, X, O):
+        return [
+            {   'number': '1',
+                'pixels':[
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, O, O, O, O, O, 
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O
+                ]
+            },
+            {   'number': '2',
+                'pixels':[
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, X, X, O,
+                    O, O, O, O, O, X, X, O,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    O, X, X, O, O, O, O, O,
+                    O, X, X, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O
+                ]
+            },
+            {   'number': '3',
+                'pixels':[
+                    O, O, O, O, O, O, X, X,
+                    O, O, O, O, O, O, X, X,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    X, X, O, O, O, O, O, O,
+                    X, X, O, O, O, O, O, O
+                ]
+            },
+            {   'number': '4',
+                'pixels':[
+                    O, O, O, O, O, O, O, O,
+                    O, X, X, O, O, X, X, O,
+                    O, X, X, O, O, X, X, O,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    O, X, X, O, O, X, X, O,
+                    O, X, X, O, O, X, X, O,
+                    O, O, O, O, O, O, O, O
+                ]
+
+            },
+            {   'number': '5',
+                'pixels':[
+                    X, X, O, O, O, O, X, X,
+                    X, X, O, O, O, O, X, X,
+                    O, O, O, O, O, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, X, X, O, O, O,
+                    O, O, O, O, O, O, O, O,
+                    X, X, O, O, O, O, X, X,
+                    X, X, O, O, O, O, X, X
+                ]
+            },
+            {   'number': '6',
+                'pixels':[
+                    O, X, X, O, O, X, X, O,
+                    O, X, X, O, O, X, X, O,
+                    O, O, O, O, O, O, O, O,
+                    O, X, X, O, O, X, X, O,
+                    O, X, X, O, O, X, X, O,
+                    O, O, O, O, O, O, O, O,
+                    O, X, X, O, O, X, X, O,
+                    O, X, X, O, O, X, X, O
+                ]
+            }
+        ]
+
+                
                 
 
-die =  ElectronicDie();
-die.checkForShake('Shake now!');
-
-
+die =  ElectronicDie()
+value = die.checkForShake('Shake now!')
+sense = SenseHat()
+sense.show_letter(value, [0, 255, 0])
+sleep(2)
+sense.clear()
