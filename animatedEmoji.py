9ad0f263a7c2ca1abbd95f31f08b7dfb8394d2f0
George Joseph <george995@gmail.com>
2020-03-31 23:07:43 +1100

    Added 3 smiley faces and made a loop to show smiley every 3 secs. Added code to clear sense hat on keyboard interrupt.

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -17,21 +17,53 @@ class AnimatedEmoji:
         R = [255, 0, 0]  # Red
         W = [255, 255, 255]  # White
         G = [0, 128, 0]  # Green
-
+        B = [0,0,128]
+        Y = [255,255,0]
         self.sense.low_light = True
 
-        smiling_face = [W, W, W, W, W, W, W, W,
-                        W, W, W, W, W, W, W, W,
-                        W, W, R, W, W, R, W, W,
-                        W, W, W, W, W, W, W, W,
-                        W, W, W, W, W, W, W, W,
-                        W, R, W, W, W, W, R, W,
-                        W, R, R, R, R, R, R, W,
-                        W, W, W, W, W, W, W, W
-                        ]
-        self.sense.set_pixels(smiling_face)
-        time.sleep(5)
-        self.sense.clear()
+        smiling_face_1 = [W, W, W, W, W, W, W, W,
+                          W, W, W, W, W, W, W, W,
+                          W, W, R, W, W, R, W, W,
+                          W, W, W, W, W, W, W, W,
+                          W, W, W, W, W, W, W, W,
+                          W, R, W, W, W, W, R, W,
+                          W, R, R, R, R, R, R, W,
+                          W, W, W, W, W, W, W, W
+                          ]
+
+        smiling_face_2 = [Y, Y, Y, Y, Y, Y, Y, Y,
+                          Y, Y, R, Y, Y, R, Y, Y,
+                          Y, Y, R, Y, Y, R, Y, Y,
+                          Y, Y, Y, Y, Y, Y, Y, Y,
+                          Y, B, Y, Y, Y, Y, B, Y,
+                          Y, B, Y, Y, Y, Y, B, Y,
+                          Y, Y, B, B, B, B, Y, Y,
+                          Y, Y, Y, Y, Y, Y, Y, Y
+                          ]
+
+        smiling_face_3 = [B, B, B, B, B, B, B, B,
+                          B, G, G, B, B, G, G, B,
+                          B, G, G, B, B, G, G, B,
+                          B, B, B, B, B, B, B, B,
+                          B, B, R, R, R, R, B, B,
+                          B, R, B, B, B, B, R, B,
+                          B, R, B, B, B, B, R, B,
+                          B, B, B, B, B, B, B, B
+                          ]
+        try:                  
+            while(True):
+                self.sense.set_pixels(smiling_face_1)
+                time.sleep(3)
+                self.sense.clear()
+                self.sense.set_pixels(smiling_face_2)
+                time.sleep(3)
+                self.sense.clear()
+                self.sense.set_pixels(smiling_face_3)
+                time.sleep(3)
+                self.sense.clear()
+        finally:
+            self.sense.clear()
+
 
 # Invoking the AnimatedEmoji class method Display
 animatedEmoji = AnimatedEmoji()
