ce2fa7d201afa01c1bde9371c46edbdbb2bcbaab
rahul94paul <rahul94.paul@gmail.com>
2020-03-31 23:21:05 +1100

    Change score count to 30

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ class Game:
         sleep(1)
         self.sense.show_message("Get ready to play...", scroll_speed=0.06, text_colour=[0, 0, 255])
         currentPlayer = self.player1
-        while(self.player1.score<=10 and self.player2.score<=10):
+        while(self.player1.score<=30 and self.player2.score<=30):
             self.performPlayerTurn(currentPlayer)
             currentPlayer = self.player2 if currentPlayer==self.player1 else self.player1
             if currentPlayer==self.player1:
@@ -41,7 +41,7 @@ class Game:
                 self.sense.show_message(self.player2.name + ":" + str(self.player2.score), scroll_speed=0.06, text_colour=[155, 155, 0])
                 print(self.player1.name + ":" + str(self.player1.score))
                 print(self.player2.name + ":" + str(self.player2.score))
-        winner = self.player1 if self.player1.score>10 else self.player2
+        winner = self.player1 if self.player1.score>30 else self.player2
         self.sense.show_message("Winner is " + winner.name + " with " + str(winner.score) + " points", scroll_speed=0.06, text_colour=[0, 255, 0])
         print("Winner is " + winner.name + " with " + str(winner.score))
         f = open('winner.csv', 'a')
