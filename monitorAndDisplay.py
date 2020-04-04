7537f2f1a154e3311f53847794743112a6748d76
George Joseph <george995@gmail.com>
2020-04-05 02:32:09 +1100

    Added boundary check for value sin config.json

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -14,10 +14,18 @@ class TemperatureMonitor:
         red = (255, 0, 0)
         blue = (0, 0, 255)
         green = (0, 255, 0)
-        
+
         # Fetching data from config.json file
         temperature_thresholds = self.get_data_from_json()
-        
+        # Checking if the boundary values in JSON are correct
+        if(not self.verify_temperature_boundary_values(temperature_thresholds)):
+            print("--------------- USER WARNING ---------------")
+            print("")
+            print("  The JSON in config.json file have invalid boundary values.")
+            print("")
+            print("--------------------------------------------")
+            exit()
+
         try:
             while(True):
                 temp = self.sense.get_temperature()
@@ -27,14 +35,14 @@ class TemperatureMonitor:
                     color = green
                 else:
                     color = red
-                self.sense.show_message(str(round(temp, 2)), text_colour = color)
+                self.sense.show_message(str(round(temp, 2)), text_colour=color)
                 time.sleep(10)
-        
+
         # Clearing sense hat display on keyboard interrupt
         finally:
             self.sense.clear()
-    
-    # Returns the JSON object from config.json 
+
+    # Returns the JSON object from config.json
     def get_data_from_json(self):
         with open('config.json') as json_file:
             try:
@@ -42,11 +50,18 @@ class TemperatureMonitor:
             except:
                 print("--------------- USER WARNING ---------------")
                 print("")
-                print("  The JSON in config.json file is invalid.")    
+                print("  The JSON in config.json file is invalid.")
                 print("")
                 print("--------------------------------------------")
                 exit()
             return data
+
+    def verify_temperature_boundary_values(self, temperature_thresholds):
+        if(temperature_thresholds['cold_max'] == temperature_thresholds['comfortable_min']
+                and temperature_thresholds['comfortable_max'] == temperature_thresholds['hot_min'] and temperature_thresholds['comfortable_min'] < temperature_thresholds['comfortable_max']):
+            return True
+        else:
+            return False
 
 
 temperature_monitor = TemperatureMonitor()
